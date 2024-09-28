49944f3873de57dc0ac604651be980f810e3d79d
Bronius <bronius256@gmail.com>
2024-09-28 21:55:28 +0300

    bytes_to_binary

--- main.py
+++ main.py
@@ -11,8 +11,17 @@ def bytes_to_hex(bytes_seq):
         hex_string += f'{b:02x}'
     return hex_string
 
+def bytes_to_binary(bytes_seq):
+    binary_string = ""
+    for b in bytes_seq:
+        binary_string += format(b, '08b')
+    return binary_string
+
+
 data = "Demo" + str(nonce)
 hashbytes = hashlib.sha256(data.encode()).digest()
+
 print(hashbytes)
 print(bytes_to_hex(hashbytes))
+print(bytes_to_binary(hashbytes))
 
