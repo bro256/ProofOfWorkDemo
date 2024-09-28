8782e932daa8054361d9652900b8d6963c48159f
Bronius <bronius256@gmail.com>
2024-09-28 22:12:09 +0300

    count_leading_zeros_binary

--- main.py
+++ main.py
@@ -17,11 +17,29 @@ def bytes_to_binary(bytes_seq):
         binary_string += format(b, '08b')
     return binary_string
 
+def count_leading_zeros_binary(binary_string):
+    count = 0
+    for char in binary_string:
+        if char == '0':
+            count += 1
+        else:
+            break
+    return count
+
+
 
 data = "Demo" + str(nonce)
 hashbytes = hashlib.sha256(data.encode()).digest()
 
+
 print("Hash bytes:", hashbytes)
-print("Hexadecimal Hash:", bytes_to_hex(hashbytes))
-print("Binary Hash:", bytes_to_binary(hashbytes))
+
+hex_hash = bytes_to_hex(hashbytes)
+print("Hexadecimal Hash:", hex_hash)
+
+binary_hash = bytes_to_binary(hashbytes)
+print("Binary Hash:", binary_hash)
+
+leading_zeros_binary = count_leading_zeros_binary(binary_hash)
+print("Leading Zeros in Binary:", leading_zeros_binary)
 
