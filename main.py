673117948054791f52b26ad5cb110c7b33298623
Bronius <bronius256@gmail.com>
2024-09-29 10:46:57 +0300

    count_leading_zeros_hex removed

--- main.py
+++ main.py
@@ -21,15 +21,6 @@ def count_leading_zeros_binary(binary_string):
             break
     return count
 
-def count_leading_zeros_hex(hex_string):
-    count = 0
-    for char in hex_string:
-        if char == '0':
-            count += 1
-        else:
-            break
-    return count
-
 def get_elapsed_time(start_time):
     return int((time.time() - start_time) * 1000)
 
@@ -41,22 +32,14 @@ while counter <= 1000000:
 
     if leading_zeros_binary > max_binary_leading_zeros:
         print("Hash bytes:", hashbytes)
-
         hex_hash = bytes_to_hex(hashbytes)
         print("Hexadecimal Hash:", hex_hash)
-
         print("Binary Hash:", binary_hash)
-
         print("Leading Zeros in Binary:", leading_zeros_binary)
-
-        leading_zeros_hex = count_leading_zeros_hex(hex_hash)
-        print("Leading Zeros in Hex:", leading_zeros_hex)
-
         print("Iterations:", counter)
-
         print('Elapsed Time:', get_elapsed_time(start_time), 'ms')
         print("------------------------------------------")
-
+        
         max_binary_leading_zeros = leading_zeros_binary
     
     nonce += 1
